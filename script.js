d38273da41d9bd14fb995e43f30d2ba3aab8691c
Ridwan Umar <lucid.exe.me@gmail.com>
2025-10-24 00:01:23 +0000

    comitted again

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const nav = document.querySelector('#nav');
+const line2 = document.querySelector('.two');
+
+function toggleMenu() {
+    line2.classList.toggle('line-width');
+    nav.classList.toggle('active');
+    
+   
+        
+ 
+}
